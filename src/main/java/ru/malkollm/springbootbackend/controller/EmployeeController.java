1ed92f80c8dabc09536c199ee0e8ddff9f6da725
Marsel <fatkullinmars23@gmail.com>
2021-01-08 11:27:29 +0500

    Подключена БД, заполнены тестовые данные, добавлен контроллер

---
+++ src/main/java/ru/malkollm/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package ru.malkollm.springbootbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.malkollm.springbootbackend.model.Employee;
+import ru.malkollm.springbootbackend.repository.EmployeeRepository;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
